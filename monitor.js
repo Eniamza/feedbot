7f1cc4169c939994122724ad0f6e235622af1dc6
Eniamza <eniamza0@gmail.com>
2023-05-07 02:15:37 +0600

    Finished for Papoi!

--- monitor.js
+++ monitor.js
@@ -1,35 +1,148 @@
-const {ethers} = require("ethers");
+const { ethers } = require("ethers");
+const axios = require('axios');
 const { sendTelegramMessage } = require("./telegram");
 const config = require("./config");
 const INFURA_API_KEY = process.env.INFURA_PROJECT_ID;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const gifUrl = "https://media.tenor.com/omNd4Vg6gSsAAAAC/minions-despicable-me.gif"
 
-const provider = new ethers.providers.InfuraProvider("homestead", INFURA_API_KEY);
-const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]; // Put the provided ABI array here
+const provider = new ethers.providers.FallbackProvider([
+  new ethers.providers.InfuraProvider("homestead", INFURA_API_KEY),
+  new ethers.providers.AlchemyProvider("homestead", ALCHEMY_API_KEY),
+  new ethers.providers.EtherscanProvider("homestead", ETHERSCAN_API_KEY),
+]);
+
+const abi = config.contractABI
 const contract = new ethers.Contract(config.contractAddress, abi, provider);
 
-const monitorBuyTransactions = async () => {
-  const eventName = "Transfer";
-  const eventFilter = contract.filters[eventName]();
-  const latestBlock = await provider.getBlockNumber();
+const pendingBuyEvents = [];
+let processingBuyEvents = false;
 
-  contract.queryFilter(eventFilter, latestBlock - 1, latestBlock).then((events) => {
+async function getWETHPriceInUSD() {
+  try {
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+    const wethPriceInUSD = response.data.ethereum.usd;
+    console.log(wethPriceInUSD);
+    return wethPriceInUSD;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function getTokenPrice() {
+  try {
+    const query = `
+      {
+        pair(id: "${config.uniswapPairAddress}") {
+          token0Price
+          token1 {
+            symbol
+          }
+          token1Price
+          token0 {
+            symbol
+          }
+        }
+      }
+    `;
+
+    const response = await axios.post("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2", { query });
+    const pairData = response.data.data.pair;
+
+    const tokenPriceInWETH = (pairData.token0.symbol === "WETH" || pairData.token0.symbol === "ETH") ? parseFloat(pairData.token0Price) : parseFloat(pairData.token1Price);
+
+    console.log(tokenPriceInWETH);
+    return tokenPriceInWETH;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+
+const processPendingBuyEvents = async () => {
+  if (processingBuyEvents || pendingBuyEvents.length === 0) return;
+
+  processingBuyEvents = true;
+  const buyEvent = pendingBuyEvents.shift();
+
+  // Pass the gifUrl as the second parameter when calling sendTelegramMessage
+  await sendTelegramMessage(buyEvent.message, gifUrl);
+  setTimeout(() => {
+    processingBuyEvents = false;
+    processPendingBuyEvents();
+  }, 5000);
+};
+
+const fetchAndProcessEvents = async () => {
+  try {
+    const eventName = "Transfer";
+    const eventFilter = contract.filters[eventName]();
+    const latestBlock = await provider.getBlockNumber();
+
+    console.log("Fetching events from block", latestBlock - 1, "to", latestBlock);
+
+    const events = await contract.queryFilter(eventFilter, latestBlock - 1, latestBlock);
+    console.log("Found", events.length, "events");
+
+    const wethPriceInUSD = await getWETHPriceInUSD();
+
     events.forEach(async (event) => {
       const { from, to, value } = event.args;
-      const amount = ethers.utils.formatEther(value);
+      const amount = parseFloat(ethers.utils.formatEther(value)).toFixed(2);
+      const tokenPriceInWETH = await getTokenPrice();
+      const tokenPriceInUSD = tokenPriceInWETH * wethPriceInUSD;
+      const amountInUSD = (amount * tokenPriceInUSD).toFixed(2);
+      const amountInETH = (amount * tokenPriceInWETH).toFixed(2)
 
-      if (config.features.alertOnBuy) {
-        const message = `🚨 *Buy Alert* 🚨
-  Someone just bought *${amount} PAINS* 
-  
+      console.log("Detected event:", {
+        from: from,
+        to: to,
+        amount: amount,
+        amountInUSD: amountInUSD,
+        amountInETH: amountInETH
+      });
+
+      console.log("Detected event:", {
+        from: from,
+        to: to,
+        amount: amount,
+        amountInUSD: amountInUSD,
+        amountInETH: amount,
+      });
+
+      if (config.features.alertOnBuy && amountInUSD >= config.minimumBuyAmountInUSD) {
+        const message = `🚨 *Buy PAPOI* 🚨
+        🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞🥞
+        
+  Someone just bought *${amount} PAPOIS* 
+  💵 ${amountInETH} ETH ($${amountInUSD})
   📥 From: [${from}](https://etherscan.io/address/${from})
   📤 To: [${to}](https://etherscan.io/address/${to})
   🔍 [View on Etherscan](https://etherscan.io/tx/${event.transactionHash})
-  💵 Buy From [UniSwap](https://app.uniswap.org/#/swap?outputCurrency=0x556d19ec20f7fffdfbe1a2c2403737ebcded96ca) or [DexTools](https://www.dextools.io/app/en/ether/pair-explorer/0x9505f89b7895c6e2ea4b0c34748caf8d128860ae)`
+  💵 Buy From [UniSwap](https://app.uniswap.org/#/swap?outputCurrency=0x556d19ec20f7fffdfbe1a2c2403737ebcded96ca) or [DexTools](https://www.dextools.io/app/en/ether/pair-explorer/0x9505f89b7895c6e2ea4b0c34748caf8d128860ae
+        )`;
 
-        await sendTelegramMessage(message);
+        console.log("Sending buy alert:", message);
+
+        pendingBuyEvents.push({message, gifUrl});
+    processPendingBuyEvents();
       }
     });
-  });
+  } catch (error) {
+    if (error.code === "SERVER_ERROR") {
+      console.log("Retry in 5 seconds due to a server error");
+      setTimeout(fetchAndProcessEvents, 5000);
+    } else {
+      console.error("Error processing events:", error);
+    }
+  }
+};
+
+const monitorBuyTransactions = async () => {
+  fetchAndProcessEvents();
 };
 
 module.exports = {
--- telegram.js
+++ telegram.js
@@ -1,16 +1,36 @@
 const axios = require("axios");
 const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } = process.env;
 
-const sendTelegramMessage = async (message) => {
-  const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
-  const response = await axios.post(telegramApiUrl, {
-    chat_id: TELEGRAM_CHAT_ID,
-    text: message,
-    parse_mode: "Markdown",
-  });
+const sendTelegramMessage = async (message, animationUrl) => {
+  try {
+    if (animationUrl) {
+      const animationPayload = {
+        chat_id: TELEGRAM_CHAT_ID,
+        animation: animationUrl,
+        caption: message,
+        parse_mode: "Markdown",
+        disable_web_page_preview: true,
+      };
 
-  return response.data;
+      const animationApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendAnimation`;
+      await axios.post(animationApiUrl, animationPayload);
+    } else {
+      const textPayload = {
+        chat_id: TELEGRAM_CHAT_ID,
+        parse_mode: "Markdown",
+        text: message,
+        disable_web_page_preview: true,
+      };
+
+      const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+      await axios.post(telegramApiUrl, textPayload);
+    }
+  } catch (error) {
+    console.error("Error sending Telegram message:", error);
+  }
 };
+
+
 
 module.exports = {
   sendTelegramMessage,
